8cdc0a21c7dc89d56de2fefd6bcdd16f6cbfbb14
allens <18856907654@#163.com>
2021-01-20 09:21:21 +0800

    协程启动模式

--- sample-coroutines/src/main/java/com/allens/sample_coroutines/MainActivity.kt
+++ sample-coroutines/src/main/java/com/allens/sample_coroutines/MainActivity.kt
@@ -13,47 +13,192 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val linear = findViewById<LinearLayout>(R.id.linear)
         linear.addView(MaterialButton(this).apply {
-            text = "启动模式 DEFAULT"
+            text = "启动模式 DEFAULT 执行Start"
             setOnClickListener {
-                todoModule(CoroutineStart.DEFAULT)
+                todoModuleDefault(isStart = true)
             }
         })
         linear.addView(MaterialButton(this).apply {
-            text = "启动模式 LAZY"
+            text = "启动模式 DEFAULT 不执行Start"
             setOnClickListener {
-                todoModule(CoroutineStart.LAZY)
+                todoModuleDefault(isStart = false)
             }
         })
         linear.addView(MaterialButton(this).apply {
-            text = "启动模式 ATOMIC"
+            text = "启动模式 LAZY 执行Start"
             setOnClickListener {
-                todoModule(CoroutineStart.ATOMIC)
+                todoModuleLazy(isStart = true)
+            }
+        })
+        linear.addView(MaterialButton(this).apply {
+            text = "启动模式 LAZY 不执行Start"
+            setOnClickListener {
+                todoModuleLazy(isStart = false)
+            }
+        })
+        linear.addView(MaterialButton(this).apply {
+            text = "启动模式 LAZY 调度前被取消"
+            setOnClickListener {
+                todoModuleLazyCancel()
+            }
+        })
+        linear.addView(MaterialButton(this).apply {
+            text = "启动模式 ATOMIC 执行Start"
+            setOnClickListener {
+                todoModuleAtomic(isStart = true)
+            }
+        })
+        linear.addView(MaterialButton(this).apply {
+            text = "启动模式 ATOMIC 不执行Start"
+            setOnClickListener {
+                todoModuleAtomic(isStart = false)
             }
         })
 
         linear.addView(MaterialButton(this).apply {
-            text = "启动模式 UNDISPATCHED"
+            text = "启动模式 UNDISPATCHED 执行Start"
             setOnClickListener {
-                todoModule(CoroutineStart.UNDISPATCHED)
+                todoModuleUnDispatched(isStart = true)
+            }
+        })
+        linear.addView(MaterialButton(this).apply {
+            text = "启动模式 UNDISPATCHED 不执行Start"
+            setOnClickListener {
+                todoModuleUnDispatched(isStart = false)
+            }
+        })
+        linear.addView(MaterialButton(this).apply {
+            text = "LAZY对比UNDISPATCHED"
+            setOnClickListener {
+                todoModuleUnDispatched(start = CoroutineStart.LAZY,isStart = true)
             }
         })
     }
 
-    private fun todoModule(start: CoroutineStart) {
+    private fun todoModuleAtomic(
+        start: CoroutineStart = CoroutineStart.ATOMIC,
+        isStart: Boolean = false
+    ) {
         for (i in 1..100) {
             val buffer = StringBuffer()
             buffer.append("start")
             val job = GlobalScope.launch(start = start) {
                 buffer.append("->执行协程")
-                delay(1000)
+                delay(100)
                 buffer.append("->协程执行完成")
             }
             buffer.append("->启动协程")
-            job.start()
+            //是否start 不会影响结果
+            if (isStart) {
+                job.start()
+            }
             //确保一定执行了 start()
-            sleep(200)
+            sleep(20)
             buffer.append("->取消协程")
             job.cancel()
+            buffer.append("->end")
+            println("第${i}次 $buffer")
+        }
+    }
+
+    private fun todoModuleUnDispatched(
+        start: CoroutineStart = CoroutineStart.UNDISPATCHED,
+        isStart: Boolean = false
+    ) {
+        for (i in 1..100) {
+            val buffer = StringBuffer()
+            buffer.append("start")
+            val job = GlobalScope.launch(start = start) {
+                buffer.append("->执行协程A")
+                buffer.append("[${Thread.currentThread().name}]")
+                delay(20)
+                buffer.append("->执行协程B")
+                buffer.append("[${Thread.currentThread().name}]")
+                delay(100)
+                buffer.append("->协程执行完成")
+            }
+            buffer.append("->启动协程")
+            //是否start 不会影响结果
+            if (isStart) {
+                job.start()
+            }
+            //确保一定执行了 start()
+            sleep(50)
+            buffer.append("->取消协程")
+            job.cancel()
+            buffer.append("->end")
+            println("第${i}次 $buffer")
+        }
+    }
+    private fun todoModuleDefault(
+        start: CoroutineStart = CoroutineStart.DEFAULT,
+        isStart: Boolean = false
+    ) {
+        for (i in 1..100) {
+            val buffer = StringBuffer()
+            buffer.append("start")
+            val job = GlobalScope.launch(start = start) {
+                buffer.append("->执行协程")
+                delay(100)
+                buffer.append("->协程执行完成")
+            }
+            buffer.append("->启动协程")
+            //是否start 不会影响结果
+            if (isStart) {
+                job.start()
+            }
+            //确保一定执行了 start()
+            sleep(20)
+            buffer.append("->取消协程")
+            job.cancel()
+            buffer.append("->end")
+            println("第${i}次 $buffer")
+        }
+    }
+
+    private fun todoModuleLazy(
+        start: CoroutineStart = CoroutineStart.LAZY,
+        isStart: Boolean = false
+    ) {
+        for (i in 1..100) {
+            val buffer = StringBuffer()
+            buffer.append("start")
+            val job = GlobalScope.launch(start = start) {
+                buffer.append("->执行协程")
+                delay(100)
+                buffer.append("->协程执行完成")
+            }
+            buffer.append("->启动协程")
+            //是否start 不会影响结果
+            if (isStart) {
+                job.start()
+            }
+            //确保一定执行了 start()
+            sleep(20)
+            buffer.append("->取消协程")
+            job.cancel()
+            buffer.append("->end")
+            println("第${i}次 $buffer")
+        }
+    }
+
+    private fun todoModuleLazyCancel(
+        start: CoroutineStart = CoroutineStart.LAZY
+    ) {
+        for (i in 1..100) {
+            val buffer = StringBuffer()
+            buffer.append("start")
+            val job = GlobalScope.launch(start = start) {
+                buffer.append("->执行协程")
+                delay(100)
+                buffer.append("->协程执行完成")
+            }
+            buffer.append("->取消协程")
+            job.cancel()
+            //确保一定执行了 cancel()
+            sleep(20)
+            buffer.append("->启动协程")
+            job.start()
             buffer.append("->end")
             println("第${i}次 $buffer")
         }
